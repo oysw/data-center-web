b8f3b2f784bd077b7780fe1428ca6da80248fa5a
oysw <swouyangcm@gmail.com>
2019-04-17 11:04:30 +0800

    upload

--- core/models.py
+++ core/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.core.files.storage import FileSystemStorage
 # Create your models here.
-
-fs = FileSystemStorage(location='/tmp/ai4chem/')
 
 
 class Job(models.Model):
@@ -13,12 +10,12 @@ class Job(models.Model):
         ('C', 'Calculating'),
     )
     owner = models.CharField(max_length=150)
-    x_file = models.FileField(blank=True, storage=fs)
-    y_file = models.FileField(blank=True, storage=fs)
+    x_file = models.FileField(blank=True, upload_to='calc_file/')
+    y_file = models.FileField(blank=True, upload_to='calc_file/')
     best = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(blank=True, storage=fs)
-    trials = models.FileField(blank=True, storage=fs)
-    create_time = models.DateTimeField(auto_now=True)
+    image = models.ImageField(blank=True, upload_to='result_file/')
+    trials = models.FileField(blank=True, upload_to='result_file/')
+    create_time = models.DateTimeField(editable=True, auto_now=True)
     status = models.CharField(max_length=10, default='W', choices=STATUS)
 
     def __str__(self):
--- dsweb/settings.py
+++ dsweb/settings.py
@@ -130,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media root
+MEDIA_ROOT = '/tmp/ai4chem/'
