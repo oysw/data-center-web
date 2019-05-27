766b609bcd4f2ab86ef76f1ffaa87d0301dccb00
oysw <swouyangcm@gmail.com>
2019-05-27 20:42:56 +0800

    bug fix

--- dsweb/settings.py
+++ dsweb/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/tmp/ai4chem/static/'
 
 # Media root
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = '/tmp/ai4chem/media/'
 
 # Center address
 DATA_CENTER = {
