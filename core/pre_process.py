2c8e21b48bac2ed5825b9a2b66507ab904a32fb7
oysw <swouyangcm@gmail.com>
2019-06-05 00:20:01 +0800

    bug fix

--- core/pre_process.py
+++ core/pre_process.py
@@ -3,6 +3,9 @@ This file is used to process csv file
 """
 import pandas as pd
 from matminer.featurizers import structure
+from matminer.featurizers import composition
+from matminer.featurizers.conversions import StructureToComposition
+from pymatgen.core.structure import Structure
 
 
 def preprocess(option, file):
@@ -31,8 +34,40 @@ def preprocess(option, file):
     else:
         structure_info = option[0]
         option = option[1:]
+        if structure_info == "rename":
+            columns = list(df.columns)
+            col_index = columns.index(option[0])
+            columns.remove(option[0])
+            columns.insert(col_index, option[1])
+            df.columns = columns
+            file.close()
+            return True, df
+        elif structure_info == "strToStructure":
+            target_col = option[0]
+            try:
+                df[target_col] = [Structure.from_str(i, 'json') for i in df[target_col]]
+            except Exception as e:
+                file.close()
+                return False, repr(e)
+            file.close()
+            return True, df
+        elif structure_info == "structureToComposition":
+            target_col = option[0]
+            try:
+                df = StructureToComposition().featurize_dataframe(df, target_col)
+            except Exception as e:
+                file.close()
+                return False, repr(e)
+            file.close()
+            return True, df
         for i in option:
-            featurizer = structure.__dict__[i]()
+            try:
+                featurizer = structure.__dict__[i]()
+            except KeyError:
+                try:
+                    featurizer = composition.__dict__[i]()
+                except KeyError:
+                    break
             try:
                 df = featurizer.featurize_dataframe(df, structure_info)
             except Exception as e:
