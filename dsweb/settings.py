2b12e029987b1a5beb0d7a13c528f6fd37ae27d1
oysw <swouyangcm@gmail.com>
2019-06-14 22:55:09 +0800

    Change database from mysql to sqlite

--- dsweb/settings.py
+++ dsweb/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'dsweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": 'django.db.backends.mysql',
-        "HOST": '127.0.0.1',
-        "PORT": '3306',
-        "NAME": 'datacenter',
-        "USER": 'root',
-        "PASSWORD": 'chem123456',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
